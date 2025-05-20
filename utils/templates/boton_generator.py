05fea0900514b9ad4adf0505cbb51bd463f8bc28
danibot1911 <andresgot11@gmail.com>
2025-05-19 20:00:13 -0500

    Update boton_generator.py

--- utils/templates/boton_generator.py
+++ utils/templates/boton_generator.py
@@ -1,6 +1,6 @@
-def generar_boton_apuesta(texto_boton="Ir a RushBet", url="https://www.rushbet.co"):
+def generar_boton_apuesta(texto, url):
     return {
         "inline_keyboard": [[
-            {"text": texto_boton, "url": url}
+            {"text": texto, "url": url}
         ]]
     }
