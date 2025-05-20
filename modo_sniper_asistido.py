660970184095ec604fb1d6284fd8731dbe115481
danibot1911 <andresgot11@gmail.com>
2025-05-20 09:25:04 -0500

    Update modo_sniper_asistido.py

--- modo_sniper_asistido.py
+++ modo_sniper_asistido.py
@@ -2,7 +2,6 @@ from utils.templates.boton_generator import generar_boton_apuesta
 from utils.analizador_ruleta import analizar_patron_ruleta
 from utils.valor_sugerido import calcular_valor_apuesta
 from utils.telegram_connector_mejorado import enviar_mensaje
-from utils.valor_sugerido import calcular_valor_apuesta
 
 def detectar_sniper_asistido(mensaje, secuencia):
     try:
@@ -21,10 +20,10 @@ def detectar_sniper_asistido(mensaje, secuencia):
             mensaje = "Patrón de 7 rojos detectado. Hora de meterle al negro con confianza nivel 4."
             nivel_confianza = 4
             valor = calcular_valor_apuesta(nivel_confianza)
-           
+
             enviar_alerta_ruleta(mensaje, valor)
-            
-            boton = generar_boton_apuesta("Abrir Ruleta", "https://rushbet.co")
+
+            boton = generar_boton_apuesta("Abrir Ruleta", "https://rushbet.co/?page=all-games&game=225")
             return {"respuesta": texto, "boton": boton}
 
         return {"respuesta": "No se detectó ningún patrón válido."}
