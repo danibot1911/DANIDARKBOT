d116c1f3dd0665f808959ff6b1c9d2da4c16ba17
danibot1911 <andresgot11@gmail.com>
2025-05-19 20:45:18 -0500

    Update telegram_connector_mejorado.py

--- utils/telegram_connector_mejorado.py
+++ utils/telegram_connector_mejorado.py
@@ -1,4 +1,3 @@
-# utils/telegram_connector_mejorado.py
 import requests
 from config import TELEGRAM_TOKEN, TELEGRAM_CHAT_ID
 
@@ -6,7 +5,9 @@ def enviar_mensaje(mensaje):
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
     data = {
         "chat_id": TELEGRAM_CHAT_ID,
-        "text": mensaje
+        "text": mensaje,
+        "parse_mode": "Markdown"
     }
-    response = requests.post(url, data=data)
+    response = requests.post(url, json=data)
+    print("Respuesta Telegram:", response.status_code, response.text)
     return response.status_code
