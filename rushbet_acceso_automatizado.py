0e7ba3f66e77200e4966729632a61fd75a949ac8
danibot1911 <andresgot11@gmail.com>
2025-05-22 15:11:26 -0500

    Delete rushbet_acceso_automatizado.py

--- rushbet_acceso_automatizado.py
+++
@@ -1,8 +0,0 @@
-def iniciar_rushbet(patron_detectado):
-    print(f"Simulando acceso a RushBet para patrón: {patron_detectado['tipo']}")
-
-    # Este es el enlace directo a la ruleta de RushBet
-    enlace_directo = "https://www.rushbet.co/?page=all-games&game=225"
-
-    # Simulamos que la IA eligió esa mesa por el patrón
-    return enlace_directo
