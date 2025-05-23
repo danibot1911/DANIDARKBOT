14affe0ebc5414120c3ecbd1a0c3f6bb5f065f3e
danibot1911 <andresgot11@gmail.com>
2025-05-22 23:02:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ load_dotenv()
 
 TOKEN = os.getenv("TELEGRAM_TOKEN")
 
+from valery_bot import manejar_mensaje_valery
+
 def main():
     app = Application.builder().token(TOKEN).build()
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, manejar_mensaje_valery))
-    print("ValeryLaMala activa.")
+    app.add_handler(MessageHandler(filters.TEXT, manejar_mensaje_valery))
     app.run_polling()
 
 if __name__ == "__main__":
